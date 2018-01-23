136283f0bff4957b06aace0d9cbaa71943950ba4
omashu <omashu@omashus-MacBook-Pro.local>
2018-01-23 16:53:52 -0500

    todo list done

---
+++ todoey/ToDoList.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  todoey
+//
+//  Created by omashu on 1/23/18.
+//  Copyright © 2018 Faith. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController{
+    
+    let itemArray = ["Find mike", "buy eggos", "destroy demmagorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+  ///Table view datasource methoods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell",for: indexPath)
+        
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+        
+    }
+    
+    
+    
+    //table view delegeate
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       /// print(itemArray[indexPath.row])
+        
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        }
+        
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+        }
+        
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+   
+
+
+}
+
